4103f837bc3f78268cf1b3c690fcf6dce7cd7562
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 07:43:03 +0000

    make files 0 - 6

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,21 +10,21 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
 
-    if (new == NULL)
-    {
-        return (NULL);
-    }
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
-    new->n = value;
+	new->n = value;
 
-    new->left = NULL;
-    new->right == NULL;
+	new->left = NULL;
+	new->right = NULL;
 
-    new->parent = parent;
+	new->parent = parent;
 
-    return (new);
+	return (new);
 }
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as left-child of another node
+ * @parent: pointer to node to insert left-child in
+ * @value: value to store in new node
+ *
+ * Return: returns pointer to created node, or NULL on fail
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL || parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = value;
+
+	new->left = NULL;
+	new->right = NULL;
+
+	new->parent = parent;
+
+	if (!parent->left)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+
+	parent->left = new;
+
+	return (new);
+}
