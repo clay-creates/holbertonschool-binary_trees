2305871fda654271f128e389a20cb1abf886af12
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 09:53:48 +0000

    updated file 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,8 +9,17 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	size_t depth = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
