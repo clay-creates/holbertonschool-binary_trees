a6e129f5e703489fdf93f245505cb20c25cac58e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 08:27:44 +0000

    make files 7 - 9 (10 in prog)

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure depth of
+ *
+ * Return: returns depth of node, or returns 0 if NULL
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (0);
+    }
+}
