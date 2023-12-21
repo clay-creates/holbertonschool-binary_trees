b3ada7993ed177ce122538142fefe0ab101652d1
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 10:55:04 +0000

    make 13 and update header

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root nofe of the tree to count nodes from
+ *
+ * Return: returns the node count, or 0 if NULL
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t node_count = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		node_count++;
+	}
+
+	node_count += binary_tree_nodes(tree->left);
+	node_count += binary_tree_nodes(tree->right);
+
+	return (node_count + 1);
+}
