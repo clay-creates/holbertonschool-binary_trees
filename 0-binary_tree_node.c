a24bc3da388f8c0afa04da6974f33474dbd43fe2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 06:36:20 +0000

    make header and 0-file

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent node of node to create
+ * @value: value to put into new node
+ *
+ * Return: returns a pointer to the new node, or NULL on fail
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+    new = malloc(sizeof(binary_tree_t));
+
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+
+    new->n = value;
+
+    new->left = NULL;
+    new->right == NULL;
+
+    new->parent = parent;
+
+    return (new);
+}
