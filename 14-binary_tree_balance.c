3f9e18f2e118360516bc694ec6bfa08633a2afd9
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 11:29:24 +0000

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,10 +9,34 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
-	{
 		return (0);
+
+	left_height = binary_tree_balance(tree->left);
+	right_height = binary_tree_balance(tree->right);
+
+	if (tree->left != NULL)
+	{
+		left_height = left_height + 1;
+	}
+	else
+	{
+		left_height = 0;
 	}
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (tree->right != NULL)
+	{
+		right_height = right_height + 1;
+	}
+	else
+	{
+		right_height = 0;
+	}
+
+	if (left_height > right_height)
+		return left_height - right_height;
+	else
+		return right_height - left_height;
 }
