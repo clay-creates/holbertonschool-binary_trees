948adb3fc489cc08dd800fea841b679c9262850b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-21 09:05:37 +0000

    mem fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,9 +12,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
 	new = malloc(sizeof(binary_tree_t));
 
-	if (new == NULL || parent == NULL)
+	if (new == NULL)
 	{
 		return (NULL);
 	}
